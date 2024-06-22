84cf7bf5de3bef68459928e418bc79896a68959e
32bitsret <dretnan@logicaladdress.com>
2024-06-22 23:07:43 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -397,6 +397,19 @@ onMounted(() => {
 })
 const mobileMenuOpen = ref(false)
 const frequency = ref(frequencies.value[0])
+<script>
+    (function(d, w, c) {
+        w.BrevoConversationsID = '666ecf046a05c93c5e7ead89';
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        var s = d.createElement('script');
+        s.async = true;
+        s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'BrevoConversations');
+</script>
+<!-- /Brevo Conversations {/literal} -->
 </script>
 <style>
 * {
